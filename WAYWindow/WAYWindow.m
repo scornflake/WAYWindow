bd0a4bbe50fb6e93d4bfb0808827cc05e5fb1e0f
Daniel Gibbs <daniel@danielgibbs.name>
2015-03-06 14:43:18 +1300

    Update tracking areas for titlebar buttons. Fixes #8

--- WAYWindow/WAYWindow.m
+++ WAYWindow/WAYWindow.m
@@ -273,6 +273,10 @@ static float kWAYWindowDefaultTrafficLightButtonsTopMargin = 0;
 		frame.origin.x = _trafficLightButtonsLeftMargin +idx*(NSWidth(frame) + 6);
 		[standardButton setFrame:frame];
 	}];
+
+    NSView *themeFrame = self.titleBarView.superview.superview;
+    [themeFrame viewWillStartLiveResize];
+    [themeFrame viewDidEndLiveResize];
 }
 
 #pragma mark - NSWindow Delegate
